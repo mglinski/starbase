ae060f2b506c0294e160f6e9b0771c13ae96e30c
Matthew Glinski <matthewglinski@gmail.com>
2014-12-21 12:51:09 -0500

    Add Bower and Gulp Refactor asset storage Add setup file for bower/gulp Modify readme

--- README.md
+++ README.md
@@ -1,15 +1,19 @@
 # EVE Starbase Configurator
 
 ## Abstract
-To provide an updatable POS builder tool because all the other ones suck or are not open source :(
+To provide an easily updated POS builder tool because all the other ones suck or are not open source :(
 
 ## Install
 To build run the following to download the latest SDE and build the static.js file.
 
-    (cd build; wget https://www.fuzzwork.co.uk/dump/sqlite-latest.sqlite.bz2; tar -xvjf sqlite-latest.sqlite.bz2; make;)
+    (cd build && wget https://www.fuzzwork.co.uk/dump/sqlite-latest.sqlite.bz2 && tar -xvjf sqlite-latest.sqlite.bz2 && make)
+    
+Starbase is now built using bower and gulp, so either run the commands in the setup.sh file or run the command blow. You will need [Node.JS and NPM](https://docs.npmjs.com/getting-started/installing-node) installed to do this.
+
+    (sudo npm install -g bower gulp && bower install && gulp)
 
 ## Contributing
 Follow git-flow for contributing back changes.
 
 ## License
-See the LICENSE file, project is opened under the MIT License.
+See the LICENSE file, project is licensed under the standard MIT License.
---
+++ gulpfile.js
@@ -0,0 +1,160 @@
+/**
+ * Starbase Gulp File
+ */
+var gulp = require('gulp'),
+	sass = require('gulp-ruby-sass'),
+	autoprefixer = require('gulp-autoprefixer'),
+	minifycss = require('gulp-minify-css'),
+	jshint = require('gulp-jshint'),
+	uglify = require('gulp-uglify'),
+	imagemin = require('gulp-imagemin'),
+	rename = require('gulp-rename'),
+	concat = require('gulp-concat'),
+	notify = require('gulp-notify'),
+	cache = require('gulp-cache'),
+	livereload = require('gulp-livereload'),
+	del = require('del'),
+	sprite = require('css-sprite').stream;
+
+/**
+ * Path Settings
+ */
+var settings = {
+	images: 'img/',
+	fonts: 'fonts/',
+	css: 'css/',
+	js: 'js/',
+	'public': 'dist/',
+	'private': 'src/',
+	bower: 'bower_packages/'
+};
+
+/**
+ * Path Object
+ */
+var paths = {
+	'dev': {
+		'css': [
+			settings.bower + 'bootstrap/dist/css/bootstrap.css',
+			settings.bower + 'fontawesome/css/font-awesome.css',
+			settings.private + settings.css + '**/*.css',
+		],
+		'js': [
+			settings.bower + 'jquery/dist/jquery.js',
+			settings.bower + 'modernizr/modernizr.js',
+			settings.bower + 'bootstrap/dist/js/bootstrap.js',
+			settings.private + settings.js + 'util.js',
+			settings.private + settings.js + 'static.js',
+			settings.private + settings.js + 'model.js',
+			settings.private + settings.js + 'ui.js',
+			//settings.private + settings.js + '**/*.js',
+		],
+		'fonts': [
+			settings.bower + 'fontawesome/fonts/*',
+			settings.bower + 'bootstrap/fonts/*',
+			settings.private + settings.fonts + '**/*'
+		],
+		'img': [
+			settings.private + settings.images + '**/*',
+		]
+	},
+	'production': {
+		'fonts': settings.public + settings.fonts,
+		'img': settings.public + settings.images,
+		'css': settings.public + settings.css,
+		'js': settings.public + settings.js,
+		'js_move': settings.public + settings.js
+	}
+};
+
+/**
+ * Styles
+ */
+gulp.task('styles', function() {
+	return gulp.src(paths.dev.css)
+		.pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
+		.pipe(gulp.dest(paths.production.css))
+		.pipe(concat('main.css'))
+		.pipe(rename({suffix: '.min'}))
+		.pipe(minifycss())
+		.pipe(gulp.dest(paths.production.css))
+		//.pipe(notify({ message: 'Styles task complete' }))
+		;
+});
+
+/**
+ * Scripts
+ */
+gulp.task('scripts', function() {
+	return gulp.src(paths.dev.js)
+		//.pipe(jshint('.jshintrc'))
+		//.pipe(jshint.reporter('default'))
+		.pipe(concat('main.js'))
+		.pipe(gulp.dest(paths.production.js))
+		.pipe(rename({suffix: '.min'}))
+		.pipe(uglify())
+		.pipe(gulp.dest(paths.production.js))
+		//.pipe(notify({ message: 'Scripts task complete' }))
+		;
+});
+
+/**
+ * Images
+ */
+gulp.task('images', function() {
+	return gulp.src(paths.dev.img)
+		.pipe(imagemin({ optimizationLevel: 3, progressive: true, interlaced: true }))
+		.pipe(gulp.dest(paths.production.img))
+		//.pipe(notify({ message: 'Images task complete' }))
+		;
+});
+
+/**
+ * Fonts
+ */
+gulp.task('fonts', function() {
+	return gulp.src(paths.dev.fonts)
+		.pipe(gulp.dest(paths.production.fonts))
+		//.pipe(notify({ message: 'Fonts task complete' }))
+		;
+});
+
+
+/**
+ * Clean
+ */
+gulp.task('clean', function(cb) {
+	del([paths.production.css, paths.production.js, paths.production.img, paths.production.fonts], cb)
+});
+
+/**
+ *
+ */
+gulp.task('default', ['clean'], function() {
+	gulp.start('styles', 'scripts', 'images', 'fonts');
+});
+
+/**
+ * Watch
+ */
+gulp.task('watch', function() {
+
+	// Watch .css files
+	gulp.watch('src/css/**/*.css', ['styles']);
+
+	// Watch .js files
+	gulp.watch('src/js/**/*.js', ['scripts']);
+
+	// Watch image files
+	gulp.watch('src/img/**/*', ['images']);
+
+	// Watch image files
+	gulp.watch('src/fonts/**/*', ['fonts']);
+
+	// Create LiveReload server
+	livereload.listen();
+
+	// Watch any files in dist/, reload on change
+	gulp.watch(['dist/**']).on('change', livereload.changed);
+
+});
