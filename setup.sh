8dfef784f8e5a3a4ba01a6dc477161564e6e0e0d
Matthew Glinski <matthewglinski@gmail.com>
2014-12-23 14:36:26 -0500

    Update readme and setup.sh

--- setup.sh
+++ setup.sh
@@ -2,9 +2,5 @@
 
 # See LICENSE file for license info
 
-sudo npm install -g bower gulp
-
-npm install
-bower install
-gulp
-
+(cd build && wget https://www.fuzzwork.co.uk/dump/sqlite-latest.sqlite.bz2 && bzip2 -d sqlite-latest.sqlite.bz2 && make)
+(npm install && sudo npm install -g bower gulp && bower install && gulp)
--- src/js/ui.js
+++ src/js/ui.js
@@ -147,7 +147,6 @@ App = (function($, model) {
 			var url = window.location;
 			$('.buildLink').val(url);
 		});
-
 		$('#getBuild').on('click', function(e){
 			e.preventDefault();
 			$('#urlModal').modal('show');
